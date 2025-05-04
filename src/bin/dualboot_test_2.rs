d0e1883885a372a3cf2ade6b65d5560e5baff53e
Fadhil Riyanto <me@fadev.org>
2025-05-04 15:51:53 +0700

    test show json from parted
    Signed-off-by: Fadhil Riyanto <me@fadev.org>

--- src/bin/dualboot_test_2.rs
+++ src/bin/dualboot_test_2.rs
@@ -3,9 +3,9 @@ use tea_partition_generator::dual_boot_blockdev::{DualBootBlockdevice, DualbootB
 #[tokio::main]
 async fn main() {
     let ctx: DualbootBlkstuff = DualBootBlockdevice::blockdevice(
-        "/dev/sdb".to_string(), 
+        "/dev/sda".to_string(), 
         "ext4".to_string() 
     );
 
-    println!("Has other os: {:#?}", ctx.check_base_disk_layout());
+    println!("partition design: {:#?}", ctx.getresult());
 }
